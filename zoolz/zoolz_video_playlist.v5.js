482b7c28c2bd0a07bfc7da845576ad0ae61b30c4
francarl <francarl@gmail.com>
2025-01-28 16:48:57 +0100

    fix

--- zoolz/zoolz_video_playlist.v5.js
+++ zoolz/zoolz_video_playlist.v5.js
@@ -109,7 +109,7 @@ $.getScript('https://cdnjs.cloudflare.com/ajax/libs/video.js/7.21.1/video.min.js
 			zoom: 1
 		};
 
-		hotkeys('alt+r,alt+z,alt+x,left,right', function (event, handler){
+		hotkeys('alt+r,alt+z,alt+x,n,m', function (event, handler){
 		  	var vi = videoplayer.children()[0];
 			switch (handler.key) {
 			    case 'alt+r':
@@ -124,11 +124,11 @@ $.getScript('https://cdnjs.cloudflare.com/ajax/libs/video.js/7.21.1/video.min.js
 					zoomrotate.zoom -= 0.1;
 					vi.style.transform = 'scale(' + zoomrotate.zoom + ') rotate(' + zoomrotate.rotate + 'deg)';		
 					break;
-				case 'left':
+				case 'n':
 					videoplayer.currentTime(videoplayer.currentTime() - 10);
 					videoplayer.play();
 					break;
-				case 'right':
+				case 'm':
 					videoplayer.currentTime(videoplayer.currentTime() + 10);
 					videoplayer.play();
 					break;
