8df1b3b1e20382bfe8feaadcc112508c1af02bbd
francarl <francarl@gmail.com>
2025-03-21 15:22:14 +0100

    removed PiP

--- zoolz/zoolz_video_playlist.v5.js
+++ zoolz/zoolz_video_playlist.v5.js
@@ -100,7 +100,10 @@ $.getScript('https://francarl.github.io/zoolz/lib/videojs/video.min.js', functio
 			  }
 		   },
 		   playbackRates: [0.1, 0.2, 0.3, 0.4, 0.5, 0.8, 1.0, 1.5, 2.0, 4.0, 8.0],
-		   muted: true
+		   muted: true,
+		   controlBar: {
+                       pictureInPictureToggle: false
+                   }
 		};
 
 		var zoomrotate = { 
