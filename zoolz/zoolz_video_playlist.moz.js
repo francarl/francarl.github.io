34c36cfdead60ef63dc8ff4a1c9666cf9ae29772
francarl <francarl@gmail.com>
2025-03-28 18:11:21 +0100

    Update zoolz_video_playlist.moz.js

--- zoolz/zoolz_video_playlist.moz.js
+++ zoolz/zoolz_video_playlist.moz.js
@@ -369,12 +369,15 @@ function attachVideo(newElem) {
 			},
 			success: function(data, textStatus, request){
 			    var durl = request.getResponseHeader('downloadUrl');
-			    // setTimeout(console.log.bind(console, durl));
-	
+			    setTimeout(console.log.bind(console, durl));
 			    newElem.attr("href", durl);
 
-				var title = durl.match(titleRegex)[1];
-				setTimeout(console.log.bind(console, title));
+			    var title = "ND";
+			    try {
+				title = durl.match(titleRegex)[1];
+			    } catch(ex) {
+				console.log(ex);
+	                    }	
 
 			    sources.push(
 				     {
