ec2b571d571c4b709d5ec4b8d2ae53dd013977a7
francarl <francarl@gmail.com>
2026-04-30 16:08:51 +0200

    no sudo

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-apt update && apt install -y openssh-server git gnupg 
-sed -i 's/^#\?Port [0-9]*/Port 8022/' /etc/ssh/sshd_config
-systemctl restart ssh
-sudo -u droid cat /mnt/shared/Download/id_ed25519.pub >> ~/.ssh/authorized_keys 
+sudo apt update && sudo apt install -y openssh-server git gnupg 
+sudo sed -i 's/^#\?Port [0-9]*/Port 8022/' /etc/ssh/sshd_config
+sudo systemctl restart ssh
+cat /mnt/shared/Download/id_ed25519.pub >> ~/.ssh/authorized_keys 
 networkctl status
+exit
