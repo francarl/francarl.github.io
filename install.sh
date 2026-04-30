561766ba1c715f657b2b9abe11ac33bad09a4f7a
francarl <francarl@gmail.com>
2026-04-30 14:01:15 +0200

    added script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+apt update && apt install -y openssh-server git gnupg 
+sed -i 's/^#\?Port [0-9]*/Port 8022/' /etc/ssh/sshd_config
+systemctl restart ssh
+cat /mnt/shared/Download/id_ed25519.pub >> ~/.ssh/authorized_keys 
+networkctl status
