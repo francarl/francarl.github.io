32787634ad424c0db15fe95714b59c40eb7b9e4e
francarl <francarl@gmail.com>
2026-05-05 15:49:26 +0200

    fix 2

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt update && sudo apt install -y openssh-server git gnupg 
+sudo apt-get update && sudo UCF_FORCE_CONFFNEW=1 apt-get install -y openssh-server git gnupg 
 sudo sed -i 's/^#\?Port [0-9]*/Port 8022/' /etc/ssh/sshd_config
 sudo systemctl restart ssh
 cat /mnt/shared/Download/id_ed25519.pub >> ~/.ssh/authorized_keys
