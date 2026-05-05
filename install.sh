389dd928232e3009604ad4904451c05c73200d29
francarl <francarl@gmail.com>
2026-05-05 15:29:34 +0200

    fix

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 sudo apt update && sudo apt install -y openssh-server git gnupg 
 sudo sed -i 's/^#\?Port [0-9]*/Port 8022/' /etc/ssh/sshd_config
 sudo systemctl restart ssh
-cat /mnt/shared/Download/id_ed25519.pub >> ~/.ssh/authorized_keys 
+cat /mnt/shared/Download/id_ed25519.pub >> ~/.ssh/authorized_keys
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv python install 3.12
+
+tar -xzvf /mnt/shared/Download/instaloader.tar.gz
+
 networkctl status
 exit
