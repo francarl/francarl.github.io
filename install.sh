7b2ede2b72d5aa2413aea620b7d8caf2c285f7fd
francarl <francarl@gmail.com>
2026-05-15 08:56:52 +0200

    Update SSH authorized_keys file with new key
    Replace id_ed25519.pub with at_id_ed25519.pub in authorized_keys.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sudo apt-get update && sudo UCF_FORCE_CONFFNEW=1 apt-get install -y openssh-server git gnupg 
 sudo sed -i 's/^#\?Port [0-9]*/Port 8022/' /etc/ssh/sshd_config
 sudo systemctl restart ssh
-cat /mnt/shared/Download/id_ed25519.pub >> ~/.ssh/authorized_keys
+cat /mnt/shared/Download/at_id_ed25519.pub >> ~/.ssh/authorized_keys
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 uv python install 3.12
